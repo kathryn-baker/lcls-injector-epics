bbdda794e0890f2b748c516d574b201d56d60ed1
Kathryn Baker <k.baker@stfc.ac.uk>
2023-05-17 16:25:48 -0700

    example enforcing use of compound PV

--- model.py
+++ model.py
@@ -1,8 +1,13 @@
+import torch
 from lume_model.torch import PyTorchModel
-from pprint import pprint
 
 
-class DebuggingPyTorchModel(PyTorchModel):
+def r_dist(x, y):
+    r = torch.sqrt(x**2 + y**2)
+    return r
+
+
+class PyTorchModelCompoundPV(PyTorchModel):
     def __init__(
         self,
         model_file,
@@ -24,8 +29,27 @@ class DebuggingPyTorchModel(PyTorchModel):
             feature_order,
             output_order,
         )
-        # print(self._output_format)
+        default_vals = []
+        for input_name in self.features:
+            if input_name in self.input_variables.keys():
+                default_vals.append(self.input_variables[input_name].default)
+            else:
+                xrms = self.input_variables["CAMR:IN20:186:XRMS"]
+                yrms = self.input_variables["CAMR:IN20:186:YRMS"]
+                default_vals.append(
+                    r_dist(torch.tensor(xrms.default), torch.tensor(yrms.default))
+                )
+        self.default_values = torch.tensor(
+            default_vals, dtype=torch.double, requires_grad=True
+        )
 
     def evaluate(self, input_dict):
-        # pprint(input_dict)
         return super().evaluate(input_dict)
+
+    def _prepare_inputs(self, input_variables):
+        model_vals = super()._prepare_inputs(input_variables)
+
+        xrms = model_vals.pop("CAMR:IN20:186:XRMS")
+        yrms = model_vals.pop("CAMR:IN20:186:YRMS")
+        model_vals["CAMR:IN20:186:R_DIST"] = r_dist(xrms, yrms).double()
+        return model_vals
--- server.py
+++ server.py
@@ -5,13 +5,12 @@ from pprint import pprint
 
 import numpy as np
 import torch
-from botorch.models.transforms.input import AffineInputTransform
 from lume_epics.epics_server import Server
 from lume_epics.utils import config_from_yaml
 from lume_model.torch import PyTorchModel
-from lume_model.utils import model_from_yaml, variables_from_yaml
+from lume_model.utils import variables_from_yaml
 
-from model import DebuggingPyTorchModel
+from model import PyTorchModelCompoundPV
 from transformers import (
     get_calibration_transformers,
     get_pv_to_sim_transformers,
@@ -37,12 +36,18 @@ if __name__ == "__main__":
     with open("configs/lcls_pv_variables.yml", "r") as f:
         input_variables, output_variables = variables_from_yaml(f)
 
-    input_pv_to_sim, output_pv_to_sim = get_pv_to_sim_transformers(
-        list(input_variables.keys()), list(output_variables.keys())
-    )
+    features = [
+        pv_info["sim_name_to_pv_name"][sim_name]
+        for sim_name in model_info["model_in_list"]
+    ]
+    outputs = [
+        pv_info["sim_name_to_pv_name"][sim_name]
+        for sim_name in model_info["model_out_list"]
+    ]
+    input_pv_to_sim, output_pv_to_sim = get_pv_to_sim_transformers(features, outputs)
     output_indices = [
         model_info["loc_out"][pv_info["pv_name_to_sim_name"][pvname]]
-        for pvname in output_variables.keys()
+        for pvname in outputs
     ]
 
     input_sim_to_nn, output_sim_to_nn = get_sim_to_nn_transformers(output_indices)
@@ -57,12 +62,12 @@ if __name__ == "__main__":
         "output_variables": output_variables,
         "input_transformers": [input_pv_to_sim, input_sim_to_nn, input_calibration],
         "output_transformers": [output_calibration, output_sim_to_nn, output_pv_to_sim],
-        "feature_order": list(input_variables.keys()),
-        "output_order": list(output_variables.keys()),
+        "feature_order": features,
+        "output_order": outputs,
         "output_format": {"type": "variable"},
     }
     server = Server(
-        DebuggingPyTorchModel, epics_config=epics_config, model_kwargs=model_kwargs
+        PyTorchModelCompoundPV, epics_config=epics_config, model_kwargs=model_kwargs
     )
 
     # monitor = False does not loop in main thread
